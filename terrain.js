ab8d863bf24c932129131578969752b176a1b1e4
Rayyan98 <ru03588@st.habib.edu.pk>
2020-11-07 23:19:01 +0500

    Flat terrain and switch btw point, wireframe and face

--- terrain.js
+++ terrain.js
@@ -1,55 +1,150 @@
-var canvas, gl;
+var canvas, gl, terrainVerts, terrainFaces, mode, rowLength
+
 
 function WebGLSetup(){
-    canvas = document.getElementById("gl-canvas");
-    gl = WebGLUtils.setupWebGL(canvas);
-    if (!gl) { alert("WebGL isn't available"); }
+    canvas = document.getElementById("gl-canvas")
+    gl = WebGLUtils.setupWebGL(canvas)
+    if (!gl) { alert("WebGL isn't available") }
     //  Configure WebGL
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0,0,0,1);
+    gl.viewport(0, 0, canvas.width, canvas.height)
+    gl.clearColor(0,0,0,1)
+    gl.enable(gl.DEPTH_TEST);
     //  Load shaders and initialize attribute buffers
-     gl.clear(gl.COLOR_BUFFER_BIT);
-    program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
-}
-function Buffer(vertices, colors){
-
-    vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
-    
-    vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-
-    cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-    
-    vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    program = initShaders(gl, "vertex-shader", "fragment-shader")
+    gl.useProgram(program)
 }
 
-function getpatch(xmin, xmax, zmin, zmax){
-    var terrainverts = []
-    for (j = zmin; j<zmax; j++){
-        for (i = xmin; i<xmax; i++){
-            terrainverts.push(vec2(i/(xmax-xmin), j/(zmax-zmin)))
-            
-            
+
+function BufferFaces(elements){    
+    iBuffer = gl.createBuffer() // Index / face buffer
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer)
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(elements), gl.STATIC_DRAW)    
+}
+
+
+function BufferVertices(vertices){
+
+    vBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW)
+    
+    vPosition = gl.getAttribLocation(program, "vPosition")
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(vPosition)
+
+    // cBuffer = gl.createBuffer();
+    // gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    
+    // vColor = gl.getAttribLocation(program, "vColor");
+    // gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    // gl.enableVertexAttribArray(vColor);
+    
+}
+
+
+function getHeight(x, z)
+{
+    return 0.0
+}
+
+
+function getPatchVert(xmin, xmax, zmin, zmax){
+
+    var terrainVerts = []
+    var step = 0.1
+    for (var z = zmin; z <= zmax; z+=step){
+        for (var x = xmin; x <= xmax; x+=step)
+        {
+            var y = getHeight(x, z)
+            terrainVerts.push(vec3(x, y, z))
         }
     }
     
-    return terrainverts
+    rowLength = Math.floor((xmax - xmin) / step) + 1
+    
+    return terrainVerts
+    
 }
 
+
+function getPatchFaces()
+{
+    var collength = terrainVerts.length / rowLength
+    var faces = []
+
+    if(mode == 0) // Point mode
+    {
+        for(var i = 0; i < terrainVerts.length; i++)
+        {
+            faces.push(i)
+        }
+    }
+    else if(mode == 1) // Wireframe mode
+    {
+        for(var col = rowLength - 1; col >= 0; col--)
+        {
+            faces.push(col)
+        }
+        for(var row = 1; row < collength; row++)
+        {
+            for(var col = 0; col < collength - 1; col++)
+            {
+                var v1 = row * rowLength + col
+                var v2 = (row - 1) * rowLength + col + 1
+                faces.push(v1, v2)
+            }
+            for(var col = (row + 1) * rowLength - 1; col >= row * rowLength; col--)
+            {
+                faces.push(col)
+            }
+        }
+    }
+    else if(mode == 2) // Face mode
+    {
+        for(var row = 0; row < collength - 1; row++)
+        {
+            for(var col = 0; col < rowLength - 1; col++)
+            {
+                var v1 = row * rowLength + col
+                var v2 = v1 + 1
+                var v3 = (row + 1) * rowLength + col
+                var v4 = v3 + 1
+                faces.push(v1, v3, v2, v4, v2, v3)
+            }
+        }
+    }
+
+    return faces
+    
+}
+
+
 window.onload = function init() {
-    WebGLSetup();
-    var terrainverts = getpatch(-10, 10, -10, 10)
-    console.log(terrainverts)
-    var terrainvertscolors = [vec4(1,1,1,1)]*terrainverts.length
-    Buffer(terrainverts, terrainvertscolors)
+    WebGLSetup()
+    mode = 1
+    terrainVerts = getPatchVert(-0.8, 0.8, -0.8, 0.8)
+    terrainFaces = getPatchFaces()
+    BufferVertices(terrainVerts)
+    BufferFaces(terrainFaces)
+    render()
+}
 
 
-}   
+function render()
+{
+    gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT)
+    if(mode == 0)
+    {
+        gl.drawElements(gl.POINTS, terrainFaces.length, gl.UNSIGNED_SHORT, 0)
+    }
+    else if (mode == 1)
+    {
+        gl.drawElements(gl.LINE_STRIP, terrainFaces.length, gl.UNSIGNED_SHORT, 0)
+    }
+    else if (mode == 2)
+    {
+        gl.drawElements(gl.TRIANGLES, terrainFaces.length, gl.UNSIGNED_SHORT, 0)
+    }
+}
